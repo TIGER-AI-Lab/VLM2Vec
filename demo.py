7ae7382643cdd97a88a291568c867f201bac52ae
Wenhu Chen <wenhuchen@uwaterloo.ca>
2024-11-01 22:41:41 -0400

    update demo

--- demo.py
+++ demo.py
@@ -6,11 +6,9 @@ from PIL import Image
 import numpy as np
 
 model_args = ModelArguments(
-    model_name='microsoft/Phi-3.5-vision-instruct',
+    model_name='TIGER-Lab/VLM2Vec-Full',
     pooling='last',
-    normalize=True,
-    lora=True,
-    checkpoint_path='TIGER-Lab/VLM2Vec-LoRA')
+    normalize=True)
 
 model = MMEBModel.load(model_args)
 model.eval()
@@ -52,3 +50,14 @@ inputs = {key: value.to('cuda') for key, value in inputs.items()}
 tgt_output = model(tgt=inputs)["tgt_reps"]
 print(string, '=', model.compute_similarity(qry_output, tgt_output))
 ## <|image_1|> Represent the given image. = tensor([[0.3105]], device='cuda:0', dtype=torch.bfloat16)
+
+inputs = processor('Find me an everyday image that matches the given caption: A cat and a tiger.',)
+inputs = {key: value.to('cuda') for key, value in inputs.items()}
+qry_output = model(qry=inputs)["qry_reps"]
+
+string = '<|image_1|> Represent the given image.'
+inputs = processor(string, [Image.open('figures/example.jpg')])
+inputs = {key: value.to('cuda') for key, value in inputs.items()}
+tgt_output = model(tgt=inputs)["tgt_reps"]
+print(string, '=', model.compute_similarity(qry_output, tgt_output))
+## <|image_1|> Represent the given image. = tensor([[0.2158]], device='cuda:0', dtype=torch.bfloat16)
