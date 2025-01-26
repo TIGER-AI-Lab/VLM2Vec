8d8ea790e51b025ae8d4d31084f27fe17c403c3b
Ziyan Jiang <ziyanjiang528@gmail.com>
2025-01-27 07:27:23 +0800

    llava train code

--- scripts/llava_next/run_train.sh
+++ scripts/llava_next/run_train.sh
@@ -1,6 +1,6 @@
-export PYTHONPATH=/home/ziyan/VLM2Vec:$PYTHONPATH
+export PYTHONPATH=/home/ziyan/VLM2Vec:$PYTHONPATH  # Change to your own path
 
-CUDA_VISIBLE_DEVICES=1,2 torchrun --nproc_per_node=2 --master_port=22447 --max_restarts=0 train.py \
+CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node=2 --master_port=22447 --max_restarts=0 train.py \
   --model_name llava-hf/llava-v1.6-mistral-7b-hf \
   --model_backbone llava_next \
   --output_dir /home/ziyan/exp/vlm2vec_llava_next_test \
@@ -13,5 +13,5 @@ CUDA_VISIBLE_DEVICES=1,2 torchrun --nproc_per_node=2 --master_port=22447 --max_r
   --max_len 64 --num_crops 1 --logging_steps 1 \
   --lr_scheduler_type linear --learning_rate 2e-5 --max_steps 2000 \
   --warmup_steps 200 --save_steps 1000 --normalize True \
-  --temperature 0.02 --per_device_train_batch_size 1 \
-  --grad_cache True --gc_q_chunk_size 2 --gc_p_chunk_size 2
+  --temperature 0.02 --per_device_train_batch_size 8 \
+  --grad_cache True --gc_q_chunk_size 1 --gc_p_chunk_size 1
