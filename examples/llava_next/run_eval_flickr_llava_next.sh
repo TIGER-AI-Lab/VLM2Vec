3d4df0c43eaafc99eeeac9cfb7ed73e88edc2c9a
Ziyan Jiang <ziyanjiang528@gmail.com>
2025-06-04 12:58:02 +0800

    remove max_len arugument

--- examples/llava_next/run_eval_flickr_llava_next.sh
+++ examples/llava_next/run_eval_flickr_llava_next.sh
@@ -4,18 +4,17 @@ export HF_HOME=~/.cache/huggingface
 CUDA_VISIBLE_DEVICES=7 python evaluation/eval_flickr.py \
   --model_name TIGER-Lab/VLM2Vec-LLaVa-Next \
   --model_backbone llava_next \
-  --max_len 256 \
   --pooling last --normalize True \
   --per_device_eval_batch_size 8 \
   --encode_output_path /home/ziyan/MMEB_eval/flickr/
 
 
 ## I -> T:
-#Recall@1: 0.9400
-#Recall@5: 0.9930
+#Recall@1: 0.9380
+#Recall@5: 0.9940
 #Recall@10: 0.9960
 #
 ## T -> I
-#Recall@1: 0.8024
-#Recall@5: 0.9494
+#Recall@1: 0.8022
+#Recall@5: 0.9496
 #Recall@10: 0.9736
