c156d11a5ae600d27e7d700ff9e480ff5f89789f
Ziyan Jiang <ziyanjiang528@gmail.com>
2025-06-04 19:02:11 +0800

    update merge lora code

--- archive/merge.py
+++ archive/merge.py
@@ -1,22 +1,17 @@
 from src.arguments import ModelArguments
-from transformers import HfArgumentParser, AutoProcessor
-
 from src.model import MMEBModel
-from evaluation.eval_utils import get_pred
 
 
 def main():
-    parser = HfArgumentParser(ModelArguments)
-    model_args, = parser.parse_args_into_dataclasses()
-    model_args: ModelArguments
-
-    processor = AutoProcessor.from_pretrained(
-        model_args.model_name,
-        trust_remote_code=True,
-        num_crops=model_args.num_crops,
+    model_args = ModelArguments(
+        model_name='Qwen/Qwen2-VL-2B-Instruct',
+        checkpoint_path='TIGER-Lab/VLM2Vec-Qwen2VL-2B',
+        pooling='last',
+        normalize=True,
+        model_backbone='qwen2_vl',
+        lora=True
     )
 
-    processor.tokenizer.padding_side = "right"
     model = MMEBModel.load(model_args)
     model.encoder._hf_peft_config_loaded = False
     model.encoder.save_pretrained('full_model/', safe_serialization=False)
