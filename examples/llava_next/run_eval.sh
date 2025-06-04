b5eb3bc6a72e3c79d1182596de60bb9d8502bd55
Ziyan Jiang <ziyanjiang528@gmail.com>
2025-06-04 13:42:12 +0800

    update llava_next eval

--- examples/llava_next/run_eval.sh
+++ examples/llava_next/run_eval.sh
@@ -1,12 +1,14 @@
 export PYTHONPATH=../VLM2Vec/:$PYTHONPATH
 
-CUDA_VISIBLE_DEVICES=0 python eval.py \
-  --model_name TIGER-Lab/VLM2Vec-LLaVA-v1.6-LoRA \
-  --image_dir DATA_DIR/MMEB_test/MMEB_Test_1K_New/images/ \
-  --encode_output_path OUTPUT_DIR/MMEB_eval/VLM2Vec-Full/ \
+CUDA_VISIBLE_DEVICES=7 python eval.py \
+  --model_name TIGER-Lab/VLM2Vec-LLaVa-Next \
+  --image_dir /data/ziyan/MMEB \
+  --encode_output_path /home/ziyan/MMEB_eval/ \
   --pooling eos --normalize True \
   --dataset_name TIGER-Lab/MMEB-eval \
   --dataset_split test \
-  --subset_name N24News ImageNet-A ImageNet-R WebQA GQA Visual7W \
+  --subset_name ImageNet-1K  \
   --image_resolution high \
-  --per_device_eval_batch_size 64
+  --per_device_eval_batch_size 8
+
+# ImageNet-1K accuracy: 0.748
