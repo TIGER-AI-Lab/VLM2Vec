68b6fdd803b17915fb32178f780aa8df92a84155
magicgh <magicgher@hotmail.com>
2025-08-24 23:18:36 +0800

    Add num_hardneg to training script

--- experiments/public/train/train_b3-qwen2vl-2B.sh
+++ experiments/public/train/train_b3-qwen2vl-2B.sh
@@ -25,7 +25,7 @@ mkdir -p $EXP_DIR/wandb
 rm -rf $EXP_DIR/wandb/*
 
 cd PATH_TO_VLM2VEC_REPO
-cmd="CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=2207 --max_restarts=0 train.py --lora --lora_r 16 --model_name Qwen/Qwen2-VL-2B-Instruct --bf16 --pooling eos --normalize True --temperature 0.02 --dataloader_num_workers 8 --dataset_config experiments/public/train/train_b3-2b.yaml --run_name $EXP_NAME --output_dir $EXP_DIR --grad_cache True --per_device_train_batch_size 128 --gc_q_chunk_size 8 --gc_p_chunk_size 8 --gc_dynamic_limit 64 --interleave_batch_size 1 --lr_scheduler_type linear --learning_rate 1e-4 --max_steps 100 --warmup_steps 10 --save_steps 10 --logging_steps 1 --save_safetensors True --remove_unused_columns False --resume_from auto --resize_use_processor True --chunk_size 32 --interleave_datasets --report_to wandb 2>&1 | tee $EXP_DIR/train.log"
+cmd="CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=2207 --max_restarts=0 train.py --lora --lora_r 16 --model_name Qwen/Qwen2-VL-2B-Instruct --bf16 --pooling eos --normalize True --temperature 0.02 --dataloader_num_workers 8 --dataset_config experiments/public/train/train_b3-2b.yaml --run_name $EXP_NAME --output_dir $EXP_DIR --grad_cache True --per_device_train_batch_size 128 --gc_q_chunk_size 8 --gc_p_chunk_size 8 --gc_dynamic_limit 64 --interleave_batch_size 1 --lr_scheduler_type linear --learning_rate 1e-4 --max_steps 100 --warmup_steps 10 --save_steps 10 --logging_steps 1 --save_safetensors True --remove_unused_columns False --resume_from auto --resize_use_processor True --chunk_size 32 --interleave_datasets --num_hardneg 3 --report_to wandb 2>&1 | tee $EXP_DIR/train.log"
 
 echo $cmd
 eval $cmd
