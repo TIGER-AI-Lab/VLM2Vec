3656571118b9a5f01099973f02bbe4e299e4879e
Santiago Castro <santiagoc@netflix.com>
2025-10-15 22:05:50 +0000

    Fix the download script to align with the eval scripts

--- experiments/public/data/download_data.sh
+++ experiments/public/data/download_data.sh
@@ -1,34 +1,56 @@
+#!/usr/bin/env bash
+
 # You may need to install git-lfs to download data properly
 
+set -ex
+
 ### Training data
+
 # 1. Change to desired directory for storing data
-DATA_BASEDIR="~/data/vlm2vec_train"
-mkdir -p $DATA_BASEDIR
-cd $DATA_BASEDIR
+DATA_BASEDIR=data/vlm2vec_train
+mkdir -p "$DATA_BASEDIR"
+pushd "$DATA_BASEDIR"
 
 # 2.1 Download image data and unzip
 git clone https://huggingface.co/datasets/TIGER-Lab/MMEB-train
-cd MMEB-train
+pushd MMEB-train
 python unzip_file.py
+popd
 
 # 2.2 Download LLaVA-Hound video data
-cd $DATA_BASEDIR
-mkdir video
-sudo apt-get install git-lfs
-git clone https://huggingface.co/datasets/ShareGPTVideo/train_video_and_instruction/
-# unzip frames in train_300k/ and train_600k/
+sudo apt-get install -y git-lfs
+git clone https://huggingface.co/datasets/ShareGPTVideo/train_video_and_instruction/ video
+pushd video/
+
+pushd train_300k/
+for f in *.tar.gz; do tar -xzvf "$f"; done
+popd
+
+pushd train_600k/
+for f in *.tar.gz; do tar -xzvf "$f"; done
+popd
+
+popd
 
 # 3. Adjust the data path in the data config yaml, refer to `experiments/public/train/train_alltasks.yaml`
 
+popd
 
 ### Eval data
-DATA_BASEDIR="~/data/vlm2vec_eval"
-mkdir -p $DATA_BASEDIR
-cd $DATA_BASEDIR
 
-git clone https://huggingface.co/datasets/TIGER-Lab/MMEB-V2
-cd MMEB-V2/image-tasks
+DATA_BASEDIR=data/vlm2vec_eval
+mkdir -p "$DATA_BASEDIR"
+pushd "$DATA_BASEDIR"
+
+git clone https://huggingface.co/datasets/TIGER-Lab/MMEB-V2 .
+pushd image-tasks/
 tar -xzvf mmeb_v1.tar.gz
+mv MMEB/* .
+rmdir MMEB
+popd
 
-cd ../video-tasks/frames
-# extract frame files accordingly
+pushd ../video-tasks/frames/
+for f in *.tar.gz; do tar -xzvf "$f" -C "${f%%.*}"; done
+popd
+
+popd
--- experiments/public/eval/eval_1gpu.sh
+++ experiments/public/eval/eval_1gpu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "==> Environment"
 echo "conda location: $(which conda)"
