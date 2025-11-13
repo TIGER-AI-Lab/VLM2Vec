3804500bbbfe7d41f2de8a895997b228da75fa70
MINGYI SU <125218610+MINGYISU@users.noreply.github.com>
2025-11-13 10:53:04 -0500

    Resolve import errors

--- eval.py
+++ eval.py
@@ -22,7 +22,7 @@ from datasets.distributed import split_dataset_by_node
 from src.arguments import ModelArguments, DataArguments, TrainingArguments
 from src.data.collator.eval_collator import MultimodalEvalDataCollator
 from src.data.eval_dataset.base_eval_dataset import AutoEvalPairDataset, generate_cand_dataset
-from src.utils.eval_utils import RankingMetrics
+from src.utils.eval_utils.metrics import RankingMetrics
 from src.model.model import MMEBModel
 from src.model.processor import get_backbone_name, load_processor, COLPALI
 from src.utils.basic_utils import batch_to_device, print_rank, print_master
