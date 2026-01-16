174808edc33df82bcbc7f0c0be56b4d71b856591
sahel <sahel.sharifi@gmail.com>
2026-01-16 12:23:01 -0500

    init must be blank for now

--- src/pyserini_integration/__init__.py
+++ src/pyserini_integration/__init__.py
@@ -1,4 +1,1 @@
-from src.pyserini_integration.vlm2vec_query_encoder import QueryEncoder
-from src.pyserini_integration.vlm2vec_corpus_encoder import CorpusEncoder
 
-__all__ = ["QueryEncoder", "CorpusEncoder"]
